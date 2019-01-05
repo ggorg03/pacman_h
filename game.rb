c74e5b8f564603ed236641709149cd3cc2d3072b
Gustavo Gorgônio <ggorgon03@gmail.com>
2019-01-04 22:10:05 -0300

    Now we can found our hero in the map

--- game.rb
+++ game.rb
@@ -1,7 +1,11 @@
 require_relative "util_game"
 
 def game
-map = load_map
+    map = load_map
 
-put map   
+    while true
+        h_position = found_hero map
+        puts h_position
+        break
+    end
 end
--- util_game.rb
+++ util_game.rb
@@ -1,3 +1,12 @@
+def found_hero map
+    map.each_with_index do |linha, n_linha|
+        n_coluna = linha.index("H")
+        if n_coluna
+            return n_coluna, n_linha
+        end
+    end
+end
+        
 def load_map
     file = File.new("map01.txt","r")
     map = file.reduce([]) {|arr, line| arr.append(line.strip)}
